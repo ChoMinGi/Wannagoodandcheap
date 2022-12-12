6ec0c14fb9663f0034641af9d8616f40d9bc6c38
Mingi Cho <81455273+ChoMinGi@users.noreply.github.com>
2022-12-12 23:47:41 +0900

    TEST spiders

--- wgnc/wgnc/spiders/test.py
+++ wgnc/wgnc/spiders/test.py
@@ -1,10 +1,19 @@
 import scrapy
 
 
-class TestSpider(scrapy.Spider):
-    name = 'test'
-    allowed_domains = ['naver.com']
-    start_urls = ['http://naver.com/']
+class QuotesSpider(scrapy.Spider):
+    name = "quotes"
+
+    def start_requests(self):
+        urls = [
+            'https://corearoadbike.com/board/board.php?g_id=recycle02&t_id=Menu01Top6&category=%ED%8C%90%EB%A7%A4&page=1',
+        ]
+        for url in urls:
+            yield scrapy.Request(url=url, callback=self.parse)
 
     def parse(self, response):
-        print('dir',dir(response))
+        page = response.url[-1]
+        filename = f'quotes-{page}.html'
+        with open(filename, 'wb') as f:
+            f.write(response.body)
+        self.log(f'Saved file {filename}')
